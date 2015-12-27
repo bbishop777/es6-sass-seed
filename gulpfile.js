ec21395fe90d347670188c239a5b6f2a00b347f2
BBishop <brad.bishop68@gmail.com>
2015-12-26 15:17:29 -1000

    Updates Babel task file to handle multiple javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,18 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var sass = require('gulp-sass');
-//Going to create a es6 task
 
-gulp.task('default', function()  {
-  return gulp.src('src/test.js')
+//es6 task
+gulp.task('babel', function()  {
+  return gulp.src('src/**/*.js')
     .pipe(babel({
       presets: ['es2015']
     }))
-    .pipe(gulp.dest('dist/combined.js'));
+    .pipe(gulp.dest('dist'));
 });
 
 
-//Also create a sass task
-
+//sass task
 gulp.task('sass', function () {
   gulp.src('scss/styles.scss')
     .pipe(sass().on('error', sass.logError))
